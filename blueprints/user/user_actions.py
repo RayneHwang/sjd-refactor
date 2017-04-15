0c912baf4431e25d4fc6c2af4dbe1e75705e2cab
Lei@lab <714582494@qq.com>
2017-04-15 17:49:12 +0800

    add password encoding

--- blueprints/user/user_actions.py
+++ blueprints/user/user_actions.py
@@ -1,16 +1,14 @@
-from flask import Flask, Blueprint, session, request
+from flask import Blueprint, session, request
+
 from models.SJD_USER import SjdUser
 from utils.db_connection import get_session
+from utils.password_encode import encode
 
-# from flask.ext.session import Session
-#
-# from app import app
-#
-# SESSION_TYPE = 'filesystem'
-# SESSION_FILE_DIR = './sess'
-# Session(app)
-#
-# session = Session()
+# Lei, HUANG: 17:41 15/04/2017
+# Flask defualt session implementation is client-side session
+# which is encrypted with app-screte key in config file
+# TODO Flask-Session uses multiple session-storafe interface
+
 
 user_module = Blueprint('user', __name__, template_folder='templates')
 
@@ -20,8 +18,8 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        mobile = request.form['mobile']
-        registered_user = get_session().query(SjdUser).filter(SjdUser.username == username).first()
+        registered_user = get_session().query(SjdUser).filter(SjdUser.username == username,
+                                                              SjdUser.password == encode(password)).first()
 
         if registered_user is None:
             return 'failed'
@@ -41,6 +39,8 @@ def login():
 @user_module.route('/status')
 def status():
     if 'username' in session:
-        return 'you are login in as %s' % session['username']
+        user = get_session().query(SjdUser).filter(SjdUser.username == session['username']).one()
+
+        return 'you are login in as %s' % user.realname
     else:
         return 'not login'
--- utils/password_encode.py
+++ utils/password_encode.py
@@ -1,1 +1,15 @@
 import hashlib
+
+
+def encode(str):
+    """
+        Encode user input password using rules inherit from legacy code
+        :arg str input password
+        :return encrypted password
+    """
+    res = hashlib.md5(str.encode()).hexdigest()
+    return res[8:24]
+
+
+if __name__ == '__main__':
+    print(encode('123456'))
